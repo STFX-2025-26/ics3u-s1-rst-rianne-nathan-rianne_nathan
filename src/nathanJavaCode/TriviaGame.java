47838a37545323c6ac6f570b387f44e8096202d0
s404118 <s404118@FXH-2441549629>
2026-01-16 08:12:36 -0500

    upd

--- src/nathanJavaCode/TriviaGame.java
+++ src/nathanJavaCode/TriviaGame.java
@@ -1,9 +1,10 @@
 package nathanJavaCode;
-
+import java.util.Scanner;
 public class TriviaGame {
 
 	public static void main(String[] args) {
 		
+		//variables
 		String[] easyQuestions = {"How many legs does a spider have?", "What is the name of the toy cowboy in Toy Story?", "What is the color of an emerald?", "Whose nose grew longer every time he lied?", "What is the name of the fairy in Peter Pan?"};
 		String[] easyAnswers = {"8", "woody", "green", "pinnochio", "tinkerbell"};
 		String[] mediumQuestions = {};
@@ -11,6 +12,36 @@ public class TriviaGame {
 		String[] hardQuestions = {};
 		String[] hardAnswers = {};
 		
+		
+		
+	}
+	
+	//DESCRIPTION - Compare the 2 string lists based on user input
+	//PARAMETERS - String[] ques , String[] answ
+	//RETURN TYPE - int
+	public static int compareQuestions(String[] ques, String[] answ) {
+		
+		//variables
+		int userScore = 0;
+		Scanner sc = new Scanner(System.in);
+		String userAns = "";
+		
+		//for loop asking questions
+		for (int i=0; i<ques.length; i++) {
+			
+			System.out.println(ques[i]);
+			userAns = sc.next();
+			
+			if (userAns.equals(answ[i])) {
+				userScore++;
+			}
+			else {
+				System.out.println("Incorrect!");
+			}
+		}
+		
+		//returning score
+		return userScore;
 	}
 
 }
