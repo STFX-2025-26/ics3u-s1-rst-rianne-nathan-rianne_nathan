1efbec42abd4726834140e0506efad8c7aedfd41
s424424 <s424424@FXH-3865184040>
2026-01-16 09:25:23 -0500

    Day 2

--- src/rianneJavaCode/TriviaGameRST.java
+++ src/rianneJavaCode/TriviaGameRST.java
@@ -9,10 +9,17 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Random;
 
 public class TriviaGameRST {
 
 	private JFrame frame;
+	private JButton Button1;
+	private JButton Button2;
+	private JButton mixedButton;
+	private JButton button4;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -54,23 +61,86 @@ public class TriviaGameRST {
 		JLabel lblNewLabel = new JLabel("Trivia");
 		lblNewLabel.setBackground(new Color(0, 0, 255));
 		lblNewLabel.setForeground(new Color(0, 0, 255));
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 21));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(188, 2, 65, 23);
+		lblNewLabel.setBounds(175, 2, 65, 23);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Choose the topic of your trivia");
 		lblNewLabel_1.setForeground(new Color(0, 0, 0));
 		lblNewLabel_1.setBackground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(145, 36, 146, 14);
+		lblNewLabel_1.setBounds(138, 36, 170, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JButton btnNewButton = new JButton("Generate question");
-		btnNewButton.setBounds(157, 107, 130, 23);
-		frame.getContentPane().add(btnNewButton);
+		JButton button4 = new JButton("Generate Question");
+		button4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// arrays for science questions and answers
+				String[] sciQuestions = new String[5];
+				sciQuestions[0] = "What is the most basic unit of the human body?";
+				sciQuestions[1] =  "What is the powerhouse of the cell?" ;
+				sciQuestions[2] =  "What is the process in which cells reproduce asexually?";
+				sciQuestions[3] = "What organelle contains the DNA of the cell?";
+				sciQuestions[4] =  "A collection of cells is known as what?";
+				
+				String[] sciAnswers = new String[5];
+				sciAnswers[0] = "Cell";
+				sciAnswers[1] = "Mitochondria";
+				sciAnswers[2] = "Mitosis";
+				sciAnswers[3] = "Nucleus";
+				sciAnswers[4] = "Tissue";
+				
+				// arrays for history questions and answers
+				String[] histQuestions = new String[5];
+				histQuestions[0] = "When was Canada founded?";
+				histQuestions[1] = "Who was the first prime minister of Canada?";
+				histQuestions[2] = "What year was the canadian penny discontinued?";
+				histQuestions[3] = "When did WWll end?";
+				histQuestions[4] = "When did WWl end?";
+				
+				String[] histAnswers = new String[5];
+				histAnswers[0] =  "1867";
+				histAnswers[1] =  "Sir John A Macdonald";
+				histAnswers[2] =  "2013";
+				histAnswers[3] =  "1917";
+				histAnswers[4] =  "1945";
+				
+				
+				int totalLength = sciQuestions.length + histQuestions.length;
+				int randomIndex = (int) (Math.random() * totalLength);
+				
+				String currentQuestion = "";
+				
+				
+				if (Button1.isSelected())
+					
+					currentQuestion = sciQuestions[(int)(Math.random() * sciQuestions.length)];
+				
+				
+				else if (Button2.isSelected())
+					
+					currentQuestion = histQuestions[(int)(Math.random() * histQuestions.length)];
+				
+				
+				else if (mixedButton.isSelected())
+					
+					if (randomIndex < sciQuestions.length)
+						currentQuestion = sciQuestions[randomIndex];
+				
+					else
+						currentQuestion = histQuestions[randomIndex - sciQuestions.length];
+				
+				textField.setText(currentQuestion);
+				textField_1.setText("");
+				
+			}
+		});
+		button4.setBounds(138, 107, 149, 23);
+		frame.getContentPane().add(button4);
 		
 		JLabel lblNewLabel_2 = new JLabel("Question:");
-		lblNewLabel_2.setBounds(68, 135, 53, 14);
+		lblNewLabel_2.setBounds(65, 135, 56, 14);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		textField = new JTextField();
@@ -79,7 +149,7 @@ public class TriviaGameRST {
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Type your answer here:");
-		lblNewLabel_3.setBounds(286, 135, 116, 14);
+		lblNewLabel_3.setBounds(254, 135, 148, 14);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		textField_1 = new JTextField();
@@ -87,28 +157,75 @@ public class TriviaGameRST {
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
-		JButton btnNewButton_1 = new JButton("Science");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnNewButton_1.setForeground(new Color(0, 0, 0));
-		btnNewButton_1.setBackground(new Color(0, 64, 0));
-		btnNewButton_1.setBounds(46, 63, 89, 23);
-		frame.getContentPane().add(btnNewButton_1);
+		JButton Button1 = new JButton("Science");
+		Button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// arrays for questions and answers
+				String[] sciQuestions = new String[5];
+				sciQuestions[0] = "What is the most basic unit of the human body?";
+				sciQuestions[1] =  "What is the powerhouse of the cell?" ;
+				sciQuestions[2] =  "What is the process in which cells reproduce asexually?";
+				sciQuestions[3] = "What organelle contains the DNA of the cell?";
+				sciQuestions[4] =  "A collection of cells is known as what?";
+				
+				String[] sciAnswers = new String[5];
+				sciAnswers[0] = "Cell";
+				sciAnswers[1] = "Mitochondria";
+				sciAnswers[2] = "Mitosis";
+				sciAnswers[3] = "Nucleus";
+				sciAnswers[4] = "Tissue";
+				
+				
+			}
+		});
+		Button1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		Button1.setForeground(new Color(0, 0, 0));
+		Button1.setBackground(new Color(0, 128, 0));
+		Button1.setBounds(46, 63, 89, 23);
+		frame.getContentPane().add(Button1);
 		
-		JButton btnNewButton_2 = new JButton("Mixed");
-		btnNewButton_2.setBounds(175, 62, 89, 23);
-		frame.getContentPane().add(btnNewButton_2);
+		JButton mixedButton = new JButton("Mixed");
+		mixedButton.setForeground(new Color(0, 0, 0));
+		mixedButton.setBackground(new Color(255, 70, 70));
+		mixedButton.setBounds(175, 62, 89, 23);
+		frame.getContentPane().add(mixedButton);
 		
-		JButton btnNewButton_3 = new JButton("History");
-		btnNewButton_3.setBounds(303, 63, 89, 23);
-		frame.getContentPane().add(btnNewButton_3);
+		JButton Button2 = new JButton("History");
+		Button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// arrays for questions and answers
+				String[] histQuestions = new String[5];
+				histQuestions[0] = "When was Canada founded?";
+				histQuestions[1] = "Who was the first prime minister of Canada?";
+				histQuestions[2] = "What year was the canadian penny discontinued?";
+				histQuestions[3] = "When did WWll end?";
+				histQuestions[4] = "When did WWl end?";
+				
+				String[] histAnswers = new String[5];
+				histAnswers[0] =  "1867";
+				histAnswers[1] =  "Sir John A Macdonald";
+				histAnswers[2] =  "2013";
+				histAnswers[3] =  "1917";
+				histAnswers[4] =  "1945";
+			}
+		});
+		Button2.setBackground(new Color(255, 128, 0));
+		Button2.setBounds(303, 63, 89, 23);
+		frame.getContentPane().add(Button2);
 		
-		JLabel lblNewLabel_4 = new JLabel("Points");
-		lblNewLabel_4.setBounds(358, 216, 34, 14);
+		JLabel lblNewLabel_4 = new JLabel("Total Points");
+		lblNewLabel_4.setBounds(313, 236, 77, 14);
 		frame.getContentPane().add(lblNewLabel_4);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(397, 213, 27, 20);
+		textField_2.setBounds(397, 233, 27, 20);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
+		
+		JButton btnNewButton_4 = new JButton("Check Answer");
+		btnNewButton_4.setBounds(264, 191, 128, 23);
+		frame.getContentPane().add(btnNewButton_4);
 	}
 }
