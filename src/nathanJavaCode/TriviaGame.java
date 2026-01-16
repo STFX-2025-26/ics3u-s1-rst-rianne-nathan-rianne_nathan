a327267f1edbf706405168b1b06c2c5e3ed49f68
s404118 <s404118@FXH-2441549629>
2026-01-16 09:11:08 -0500

    upd6

--- src/nathanJavaCode/TriviaGame.java
+++ src/nathanJavaCode/TriviaGame.java
@@ -5,12 +5,12 @@ public class TriviaGame {
 	public static void main(String[] args) {
 		
 		//variables
-		String[] easyQuestions = {"How many legs does a spider have?", "What is the name of the toy cowboy in Toy Story?", "What is the color of an emerald?", "Whose nose grew longer every time he lied?", "What is the name of the fairy in Peter Pan?"};
-		String[] easyAnswers = {"8", "woody", "green", "pinnochio", "tinkerbell"};
-		String[] mediumQuestions = {};
-		String[] mediumAnswers = {};
-		String[] hardQuestions = {};
-		String[] hardAnswers = {};
+		String[] easyQuestions = {"How many legs does a spider have?", "What is the name of the toy cowboy in Toy Story?", "What is the color of an emerald?", "Who wrote Romeo and Juliet?", "What is the name of the fairy in Peter Pan?"};
+		String[] easyAnswers = {"8", "woody", "green", "shakspeare", "tinkerbell"};
+		String[] mediumQuestions = {"What's the capital of Australia?", "What's the chemical symbol for Gold?", "Who painted The Starry Night?", "Whose nose grew longer every time he lied?", "What is the world's longest river?"};
+		String[] mediumAnswers = {"canberra", "au", "vanh gogh", "pinnochio", "nile"};
+		String[] hardQuestions = {"In which country can you find the world's largest escalator system?", "Which dinosaur's name means \"speedy thief\"?", "In what year was the Gregorian calendar adopted?", "Which animation technique involves tracing over live-action footage to create more realistic character movements?", "Which branch of mathematics gets its name from the Arabic word for \"reuniting\"?"};
+		String[] hardAnswers = {"hong kong", "velociraptor", "1582", "rotoscoping", "algebra"};
 		int correctPoints;
 		int incorrectPoints;
 		double correctMark;
@@ -36,11 +36,22 @@ public class TriviaGame {
 				userInput = 4;
 			}
 			else if (userInput == 2) {
+				correctPoints = compareQuestions(mediumQuestions, mediumAnswers);
+				incorrectPoints = mediumQuestions.length - correctPoints;
+				correctMark = percentCalc(correctPoints, mediumQuestions.length);
+				incorrectMark = percentCalc(incorrectPoints, mediumQuestions.length);
+				results(correctPoints, incorrectPoints, correctMark, incorrectMark);
+				userInput = 4;
 				
 			}
 			
 			else if (userInput == 3) {
-				
+				correctPoints = compareQuestions(hardQuestions, hardAnswers);
+				incorrectPoints = hardQuestions.length - correctPoints;
+				correctMark = percentCalc(correctPoints, hardQuestions.length);
+				incorrectMark = percentCalc(incorrectPoints, hardQuestions.length);
+				results(correctPoints, incorrectPoints, correctMark, incorrectMark);
+				userInput = 4;
 			}
 			
 			else if (userInput == 4) {
