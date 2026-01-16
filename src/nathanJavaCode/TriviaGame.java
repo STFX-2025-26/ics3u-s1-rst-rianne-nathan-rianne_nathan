8169de284d524fb14ff743619a4494e6df9f0782
s404118 <s404118@FXH-2441549629>
2026-01-16 08:00:28 -0500

    push again

--- src/nathanJavaCode/TriviaGame.java
+++ src/nathanJavaCode/TriviaGame.java
@@ -4,7 +4,13 @@ public class TriviaGame {
 
 	public static void main(String[] args) {
 		
-
+		String[] easyQuestions = {"How many legs does a spider have?", "What is the name of the toy cowboy in Toy Story?", "What is the color of an emerald?", "Whose nose grew longer every time he lied?", "What is the name of the fairy in Peter Pan?"};
+		String[] easyAnswers = {"8", "woody", "green", "pinnochio", "tinkerbell"};
+		String[] mediumQuestions = {};
+		String[] mediumAnswers = {};
+		String[] hardQuestions = {};
+		String[] hardAnswers = {};
+		
 	}
 
 }
